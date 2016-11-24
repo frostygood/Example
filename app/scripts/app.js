189d56aa5d6bcc5a5ded4f07d413c8a31d159d42
Frosty <AVinkovatov@keaz.ru>
2016-11-24 14:43:37 +0300

    мозг кипит

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,97 +7,85 @@ $(() => {
 });
 
 $(() => {
-	$.fn.validateName = function(configName){
-		$(this).on('keyup', function (){
-			var error = [];
-			var value = $(this).val().trim();
-			if (('maxLenght' in configName) && (value.length > configName.maxLenght) ) {
-				error.push('больше 100 символов ввести нельзя');
+	$.fn.validateForm = function(config){
+	var form= $(this).closest('form');
+
+const _check=function (){
+	var error = [];
+	var value = $(this).val().trim();
+
+	if (('maxLenght' in config) && (value.length > config.maxLenght.value) ) {
+		error.push(config.maxLenght.message);
+	}
+	if (('required' in config) && (!!value.length != config.required.value)) {
+		error.push(config.required.message);
+	} else if (('minLenght' in config) && (value.length < config.minLenght.value)) {
+		error.push(config.minLenght.message);
+	}
+
+	if (('pattern' in config)) {
+		var patterns=config.pattern;
+		if(!(patterns instanceof Array)){
+			patterns=[patterns];
+		}
+		for(var i=0;i<patterns.length;i++){
+			var pattern=patterns[i];
+			if(pattern.value.test(value)){
+				error.push(pattern.message);
 			}
-			$(configName.target).text(error.join(' '));
-			$(this).toggleClass(configName.classError, error.length > 0);
+		}
+	}
+
+		var isValid=error.length > 0;
+		$(config.target).text(error[error.length-1]);
+		$(this).toggleClass(config.classError, isValid);
+		$(form).toggleClass(config.classError, isValid);
+	};
+
+	$(this).on('keyup', _check);
+
+		$(form).on('submit',function(_event){ //перехват события отправки формы, перед отправкой обрабатываем содержимое внутри
+			_check();//принудительный запуск валидации
+
+			if($(this).hasClass(config.classError)){ //проверили наличие класса ошибки из конфига на ФОРМЕ
+				_event.preventDefault(); //отменяет действие по умолчанию (отправку формы, если есть класс ошибки)
+			}
 		});
 	};
-	$.fn.validatePass = function(configPass){
-		$(this).on('keyup', function (){
-			var error = [];
-			var value = $(this).val().trim();
-			if (('maxLenght' in configPass) && (value.length > configPass.maxLenght) ) {
-				error.push('больше 100 символов ввести нельзя');
-			}
-			if (('minLenght' in configPass) && (value.length < configPass.minLenght) ) {
-				error.push('Пароль не может быть короче 6 символов');
-			}
-			$(configPass.target).text(error.join(' '));
-			$(this).toggleClass(configPass.classError, error.length > 0);
-		});
-	};
-	$.fn.validatePhone = function(configPhone){
-		$(this).on('keyup', function (){
-			var error = [];
-			var value = $(this).val().trim();
-			if (('maxLenght' in configPhone) && (value.length > configPhone.maxLenght) ) {
-				error.push('больше 100 символов ввести нельзя');
-			}
-			$(configPhone.target).text(error.join(' '));
-			$(this).toggleClass(configPhone.classError, error.length > 0);
-		});
-	};
-	$.fn.validateCompany = function(configCompany){
-		$(this).on('keyup', function (){
-			var error = [];
-			var value = $(this).val().trim();
-			if (('maxLenght' in configCompany) && (value.length > configCompany.maxLenght) ) {
-				error.push('больше 100 символов ввести нельзя');
-			}
-			if (('minLenght' in configCompany) && (value.length < configCompany.minLenght) ) {
-				error.push('Название компании не может быть короче 2 символов/The company name cannot be shorter than 2 symbols');
-			}
-			$(configCompany.target).text(error.join(' '));
-			$(this).toggleClass(configCompany.classError, error.length > 0);
-		});
-	};
-	$.fn.validateEmail = function(configEmail){
-		$(this).on('keyup', function (){
-			var error = [];
-			var value = $(this).val().trim();
-			if (('maxLenght' in configEmail) && (value.length > configEmail.maxLenght) ) {
-				error.push('больше 318 символов ввести нельзя');
-			}
-			$(configEmail.target).text(error.join(' '));
-			$(this).toggleClass(configEmail.classError, error.length > 0);
-		});
-	};
+
 	var configName = {
-		maxLenght : 10,
+		maxLenght : {value:10 , message:'больше ввести нельзя '},
+		minLenght : {value:3 , message:'меньше ввести нельзя '},
+	 	required : {value:true , message:'Пожалуйста, укажите имя и фамилию/Please specify your first and last name'},
+		pattern : {value:/^0/gim, message:'Введите телефонный номер в международном формате'},
 		target : '.js-errorName',
 		classError : 'error-for-form'
 	};
-	var configPass = {
-		maxLenght : 10,
-		minLenght : 6,
-		target : '.js-errorPass',
-		classError : 'error-for-form'
-	};
-	var configPhone = {
-		maxLenght : 10,
-		target : '.js-errorPhone',
-		classError : 'error-for-form'
-	};
-	var configCompany = {
-		maxLenght : 10,
-		minLenght : 2,
-		target : '.js-errorCompany',
-		classError : 'error-for-form'
-	};
-	var configEmail = {
-		maxLenght : 18,
-		target : '.js-errorEmail',
-		classError : 'error-for-form'
-	};
-	$('.js-inputName').validateName(configName);
-	$('.js-inputPass').validatePass(configPass);
-	$('.js-inputPhone').validatePhone(configPhone);
-	$('.js-inputCompany').validateCompany(configCompany);
-	$('.js-inputEmail').validateCompany(configEmail);
+	// var configPass = {
+	// 	maxLenght : 10,
+	// 	minLenght : 6,
+	// 	target : '.js-errorPass',
+	// 	classError : 'error-for-form'
+	// };
+	// var configPhone = {
+	// 	maxLenght : 10,
+	// 	target : '.js-errorPhone',
+	// 	classError : 'error-for-form'
+	// };
+	// var configCompany = {
+	// 	maxLenght : 10,
+	// 	minLenght : 2,
+	// 	target : '.js-errorCompany',
+	// 	classError : 'error-for-form'
+	// };
+	// var configEmail = {
+	// 	maxLenght : 18,
+	// 	target : '.js-errorEmail',
+	// 	classError : 'error-for-form'
+	// };
+	$('.js-inputName').validateForm(configName);
+	// $('.js-inputPass').validateForm(configPass);
+	// $('.js-inputPhone').validateForm(configPhone);
+	// $('.js-inputCompany').validateForm(configCompany);
+	// $('.js-inputEmail').validateForm(configEmail);
 });
